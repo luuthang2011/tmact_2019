2df40ee3e5b77d484dfe93ae03ec00049364baf4
luuthang2011 <luuthang2011@gmail.com>
2019-08-09 15:51:52 +0700

    Added update value

--- Libs/feature2geodatabase.py
+++ Libs/feature2geodatabase.py
@@ -53,15 +53,11 @@ class layer2DB:
                 # MS SQL table name
                 ms_table = 'CSDLTayBac.dbo.Tbl_fc_magma'
                 # Insert Multiple database to MS SQL
-                # Add layername and layerid column
-                # columns += ['layername', 'layerid']
-                # print columns
                 msServer.multiple_insert(ms_table, columns, pg_results)
-                field = 'layername'
-                msServer.update_value_null(ms_table, field, 'layer_name_them_o_day')
-                field = 'layerid'
+                # Add layername and layerid column
+                msServer.update_value_null(ms_table, 'layername', 'layer_name_them_o_day')
                 # value = 'layer_id_them_o_day'
-                msServer.update_value_null(ms_table, field, 'layer_id_them_o_day')
+                msServer.update_value_null(ms_table, 'layerid', 'layer_id_them_o_day')
 
                 # end import data to db for CMS
 
