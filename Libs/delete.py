390153e95a9a9a7d1314abc3a8ed3df515d0e0e7
thanglq <thanglq@fimo.edu.vn>
2020-09-13 01:52:20 +0700

    update delete try all main

--- Libs/delete.py
+++ Libs/delete.py
@@ -84,40 +84,48 @@ class Delete:
 
 
 if __name__ == '__main__':
-    unitest = Delete()
-    # ex: http://<server name>:6080/arcgis/admin
-    server = constant.server  # fixed
-    admin = constant.admin  # fixed
-    password = constant.password  # fixed
+    try:
+        unitest = Delete()
+        # ex: http://<server name>:6080/arcgis/admin
+        server = constant.server  # fixed
+        admin = constant.admin  # fixed
+        password = constant.password  # fixed
 
-    # root = r'E:\SourceCode\tmact_2019\data\mdb\\' # fixed
+        # root = r'E:\SourceCode\tmact_2019\data\mdb\\' # fixed
 
-    print 'Argument List:', str(sys.argv)
+        print 'Argument List:', str(sys.argv)
 
-    service = "Magma"  # from DB
-    folder = r'E:/SourceCode/tmact_2019/data/mdb/1572371062527/'
-    mxd = r'E:/SourceCode/tmact_2019/data/mdb/1572371062527/prepare.mxd'
-    ms_table = 'Tbl_FC_Magma'
+        service = "Magma"  # from DB
+        folder = r'E:/SourceCode/tmact_2019/data/mdb/1572371062527/'
+        mxd = r'E:/SourceCode/tmact_2019/data/mdb/1572371062527/prepare.mxd'
+        ms_table = 'Tbl_FC_Magma'
 
-    # service = sys.argv[1]  # from DB
-    # folder = sys.argv[2]
-    # mxd = sys.argv[3]
-    # ms_table = sys.argv[4]
+        # service = sys.argv[1]  # from DB
+        # folder = sys.argv[2]
+        # mxd = sys.argv[3]
+        # ms_table = sys.argv[4]
 
-    print 'Start delete Rabbit!'
-    unitest.deleteRabbit(mxd, ms_table, service)
+        print 'Start delete Rabbit!'
+        unitest.deleteRabbit(mxd, ms_table, service)
 
-    # if you need a token, execute this line:
+        # if you need a token, execute this line:
+        print 'Start delete MSSQL!'
+        unitest.deleteMSSQL(ms_table, service)
 
-    print 'Start delete MongoDB!'
-    unitest.deleteMongo(service)
-    print 'Start delete MSSQL!'
-    unitest.deleteMSSQL(ms_table, service)
+        print 'Start delete Service!'
+        unitest.deleteservice(server, service + ".MapServer", admin, password)
+        print 'Start delete PostgreDB!'
+        unitest.deleteDB(mxd)
+        # print 'Start delete Directory!'
+        # unitest.deleteDir(folder)
 
-    print 'Start delete Service!'
-    unitest.deleteservice(server, service + ".MapServer", admin, password)
-    print 'Start delete PostgreDB!'
-    unitest.deleteDB(mxd)
-    # print 'Start delete Directory!'
-    # unitest.deleteDir(folder)
-    print 'All done'
+        print 'Start delete MongoDB!'
+        unitest.deleteMongo(service)
+
+        print 'All done'
+    except Exception, e:
+        print("An exception occurred")
+        print e.message
+        print "----------------------------------------------"
+        print "----Flow end. Exception. Pls check errors!----"
+        print "----------------------------------------------"
